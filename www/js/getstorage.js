a291a7f6d81c7fed273dafcfce546c8da998b9fa
jugurdzija <sasa.jugurdzija76@gmail.com>
2014-06-07 21:41:42 +0200

    get storage js 4

--- www/js/getstorage.js
+++ www/js/getstorage.js
@@ -1,20 +1,16 @@
-    function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
-    }
-		  function onDeviceReady() {
-        // Now safe to use the Cordova API
+	$("document").ready(function (){
 
 		if (window.localStorage.getItem("xylitol_th") == "true"){
 			$("#xylitol_button").show();
 		 } else {
 			$("#xylitol_button").hide();
 		 }
-		 
+
 		 if (window.localStorage.getItem("chlorhexidine_th") == "true"){
 			$("#chlorhexidine_button").show();
 		 } else {
 			$("#chlorhexidine_button").hide();
 		 }
-    }
+    });
 
      
