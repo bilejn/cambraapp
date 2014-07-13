1fd439fc0a5f00776084881ba10bc079b8a6138b
jugurdzija <sasa.jugurdzija76@gmail.com>
2014-07-13 18:01:07 +0200

    registration and timer finished

--- www/js/timer.js
+++ www/js/timer.js
@@ -1,19 +1,33 @@
-function timer (){
+function timer (interval){
+	$("#timer-button").addClass('ui-disabled');
+	var secTrue= interval;
+	var sec;
+	var min;
+	var zero;
 	
-	i=0;
-	document.getElementById("timer-button").onclick="";
-	var TeethBrushingTimer = setInterval(function(){
-	
-			document.getElementById("timer").style.width=i+"%";
-			i=i+1;
-			if (i==100){
-				registration('teeth_brushing_done'); 
-				document.getElementById("timer-button").onclick="timer()";
-				clearInterval(TeethBrushingTimer);
-				i=0;
-			}
+function delayedLoop(){
+	min = Math.floor(secTrue / 60);
+	sec = secTrue % 60;
 
-	},100)
+
+	$("#timer").html(pad2(min)+":"+pad2(sec));
+	secTrue = secTrue - 1;
+
+	if (secTrue<0) { 
+	$("#timer").html("00:00");
+	registration("teeth_brushing_done");
+	$("#timer-button").removeClass('ui-disabled'); 
+	$("#timer").html("02:00");
+	return; 
+	}
+	window.setTimeout(delayedLoop, 1000);
+}
+delayedLoop();
 			
+}
 
+function pad2(n){
+
+  return n < 10 ? '0' + n : n;
+  
 }
